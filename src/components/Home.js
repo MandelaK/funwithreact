83fcf7f66675a95a1987fcd17eaf818c9b2f32da
MandelaK <kazi.keith@gmail.com>
2019-04-09 06:33:35 +0300

    feat(home) home sweet homepage
    - add homepage to the app

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="container">
+      <p>
+        Let's have some fun!!! Click on either of the links above the navbar!
+      </p>
+    </div>
+  );
+};
+ export default Home;
